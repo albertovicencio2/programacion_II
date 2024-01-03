6e70231be65992e468bf16c373f1f25807559ce2
albertovicencio2 <alberto.vicencio@itsna.edu.mx>
2024-01-03 15:59:01 -0600

    first commit

---
+++ area_triangulo.py
@@ -0,0 +1,10 @@
+# Definir la base y la altura del triángulo
+base = float(input("Ingrese la longitud de la base del triángulo: "))
+altura = float(input("Ingrese la altura del triángulo: "))
+
+# Calcular el área del triángulo
+area = 0.5 * base * altura
+
+# Imprimir el resultado
+print("El área del triángulo es:", area)
+
---
+++ suma.py
@@ -0,0 +1,9 @@
+# Definir dos números
+numero1 = 5
+numero2 = 10
+
+# Sumar los dos números
+resultado = numero1 + numero2
+
+# Imprimir el resultado
+print("La suma de", numero1, "y", numero2, "es:", resultado)
